c725a521e7b1c89c40e44b5b8065422cafba2c8e
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-10-22 14:05:01 -0400

    Create AuthService class

---
+++ lib/services/auth.dart
@@ -0,0 +1,33 @@
+arimport 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  /// Signing in ///
+
+  // sign in email
+
+  // sign in google
+
+  // sign in apple
+
+  // sign in twitter
+
+  // sign in github
+
+  /// Signing up ///
+
+  // sign up email
+
+  // sign up google
+
+  // sign up apple
+
+  // sign up twitter
+
+  // sign up github
+
+  /// Signing out ///
+
+  // sign out
+}
