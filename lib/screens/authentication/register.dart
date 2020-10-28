476352010233bb3933e33bcc527c06ddd0e36f75
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-10-28 09:18:25 -0400

    Change layout of sign in and register  page to prevent overflow

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -38,93 +38,119 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
         padding: EdgeInsets.all(20.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                textInputAction: TextInputAction.next,
-                validator: (value) =>
-                    value.isEmpty ? 'Please enter your email' : null,
-                onChanged: (value) {
-                  setState(() {
-                    _email = value;
-                  });
-                },
-                decoration: authFieldDecoration.copyWith(
-                  icon: Icon(Icons.alternate_email),
-                  labelText: 'Email',
-                  hintText: 'email@example.com',
-                ),
-              ),
-              SizedBox(height: 10.0),
-              TextFormField(
-                obscureText: true,
-                textInputAction: TextInputAction.done,
-                validator: (value) => value.length < 6
-                    ? 'Please enter a password longer than 5 characters'
-                    : null,
-                onChanged: (value) {
-                  setState(() {
-                    _password = value;
-                  });
-                },
-                onFieldSubmitted: (_) => _validateForm,
-                decoration: authFieldDecoration.copyWith(
-                  icon: Icon(Icons.lock_open),
-                  labelText: 'Password',
-                  hintText: '●●●●●●●●●●●●',
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0),
-                child: ElevatedButton(
-                  onPressed: _validateForm,
-                  child: Text('Register'),
-                ),
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      margin: const EdgeInsets.only(left: 10.0, right: 20.0),
-                      child: Divider(height: 36),
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Price Tracker',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 45.0,
+                        fontWeight: FontWeight.w900,
+                      ),
                     ),
-                  ),
-                  Text("OR"),
-                  Expanded(
-                    child: Container(
-                      margin: const EdgeInsets.only(left: 20.0, right: 10.0),
-                      child: Divider(height: 36),
+                    SizedBox(height: 50.0),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.next,
+                      validator: (value) =>
+                          value.isEmpty ? 'Please enter your email' : null,
+                      onChanged: (value) {
+                        setState(() {
+                          _email = value;
+                        });
+                      },
+                      decoration: authFieldDecoration.copyWith(
+                        icon: Icon(Icons.alternate_email),
+                        labelText: 'Email',
+                        hintText: 'email@example.com',
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                      obscureText: true,
+                      textInputAction: TextInputAction.done,
+                      validator: (value) => value.length < 6
+                          ? 'Please enter a password longer than 5 characters'
+                          : null,
+                      onChanged: (value) {
+                        setState(() {
+                          _password = value;
+                        });
+                      },
+                      onFieldSubmitted: (_) => _validateForm,
+                      decoration: authFieldDecoration.copyWith(
+                        icon: Icon(Icons.lock_open),
+                        labelText: 'Password',
+                        hintText: '●●●●●●●●●●●●',
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      child: ElevatedButton(
+                        onPressed: _validateForm,
+                        child: Text('Register'),
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            margin:
+                                const EdgeInsets.only(left: 10.0, right: 20.0),
+                            child: Divider(height: 36),
+                          ),
+                        ),
+                        Text("OR"),
+                        Expanded(
+                          child: Container(
+                            margin:
+                                const EdgeInsets.only(left: 20.0, right: 10.0),
+                            child: Divider(height: 36),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SignInButton(
+                      Buttons.Google,
+                      onPressed: () async {
+                        await _auth.signInWithGoogle();
+                      },
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <SignInButton>[
+                        SignInButton(
+                          Buttons.Twitter,
+                          mini: true,
+                          onPressed: () async {
+                            await _auth.signInWithTwitter();
+                          },
+                        ),
+                        SignInButton(
+                          Buttons.Apple,
+                          mini: true,
+                          onPressed: () async {
+                            await _auth.signInWithApple();
+                          },
+                        ),
+                        SignInButton(
+                          Buttons.GitHub,
+                          mini: true,
+                          onPressed: () async {
+                            await _auth.signInWithGitHub();
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              SignInButton(
-                Buttons.Google,
-                onPressed: () async {
-                  await _auth.signInWithGoogle();
-                },
-              ),
-              SignInButton(
-                Buttons.Twitter,
-                onPressed: () async {
-                  await _auth.signInWithTwitter();
-                },
-              ),
-              SignInButton(
-                Buttons.Apple,
-                onPressed: () async {
-                  await _auth.signInWithApple();
-                },
-              ),
-              SignInButton(
-                Buttons.GitHub,
-                onPressed: () async {
-                  await _auth.signInWithGitHub();
-                },
-              )
             ],
           ),
         ),
