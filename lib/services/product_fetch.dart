6fc46f3498bf93632b5c0ec2eefa229740f74ec6
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-11-25 09:25:46 -0500

    Update data fetching with try/catch

--- lib/services/product_fetch.dart
+++ lib/services/product_fetch.dart
@@ -6,10 +6,14 @@ class ProductModel {
   Future<dynamic> getProductData(int productSKU) async {
     NetworkHelper networkHelper =
         NetworkHelper('$bestBuyCanadaURL/$productSKU');
-    var productData = await networkHelper.getData();
-    // TODO: Remove these temporary prints
-    print('Regular price: ${productData['regularPrice']}');
-    print('Sale price: ${productData['salePrice']}');
-    return productData;
+    try {
+      var productData = await networkHelper.getData();
+      // TODO: Remove these temporary prints
+      print('Regular price: ${productData['regularPrice']}');
+      print('Sale price: ${productData['salePrice']}');
+      return productData;
+    } catch (e) {
+      print(e);
+    }
   }
 }
