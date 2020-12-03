fac4f5ca21c03ef5ec835a3665a2940b6293cab7
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-12-03 13:36:57 -0500

    New product list widget

--- lib/screens/home/views/product_list.dart
+++ lib/screens/home/views/product_list.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:price_tracker/services/firestore.dart';
 import 'package:price_tracker/screens/home/components/product.dart';
 
-class ProductList extends StatefulWidget {
-  @override
-  _ProductListState createState() => _ProductListState();
-}
-
-class _ProductListState extends State<ProductList> {
-  final List<int> skuList = <int>[14672700, 14584744];
+class ProductList extends StatelessWidget {
+  final FirestoreService _db = FirestoreService();
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: const EdgeInsets.all(15.0),
-        itemCount: skuList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Product(skuList[index]);
-        });
+    return StreamBuilder<QuerySnapshot>(
+      stream: _db.getTrackedProducts(context),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text('Loading');
+        }
+
+        return ListView(
+          padding: const EdgeInsets.all(15.0),
+          children: snapshot.data.docs.map((DocumentSnapshot document) {
+            return Card(
+              child: ListTile(
+                leading: Image(
+                  image: NetworkImage(document.data()['productImage']),
+                ),
+                title:
+                    Text('Prix de vente : ${document.data()['lastSalePrice']}'),
+                subtitle: Text(
+                    'Prix régulier : ${document.data()['lastRegularPrice']}'),
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
   }
 }
