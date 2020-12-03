ae22431f71e00c2afdf98f5a14a4a5d5c4c87778
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-12-03 13:35:46 -0500

    Add Stream for tracked products

---
+++ lib/services/firestore.dart
@@ -0,0 +1,17 @@
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Stream<QuerySnapshot> getTrackedProducts(context) {
+    User currentUser = Provider.of<User>(context);
+    String uid = currentUser.uid;
+    Stream<QuerySnapshot> productsCollection =
+        _db.collection('users').doc(uid).collection('products').snapshots();
+
+    print(productsCollection);
+    return productsCollection;
+  }
+}
