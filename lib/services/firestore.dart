074ba7cb002a0ef967e76e8079a1e2de27caacb4
Jakob Bouchard <24767889+jakobbouchard@users.noreply.github.com>
2020-12-07 11:06:49 -0500

    Better user detection

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,12 +1,13 @@
-import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:price_tracker/services/auth.dart';
 
 class FirestoreService {
+  final AuthService _auth = AuthService();
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
   Stream<QuerySnapshot> getTrackedProducts(context) {
-    User currentUser = Provider.of<User>(context);
+    User currentUser = _auth.currentUser;
     String uid = currentUser.uid;
     Stream<QuerySnapshot> productsCollection = _db
         .collection('users')
